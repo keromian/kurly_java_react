2b135b296f35c95e967d0177ac801ef2e26bd75a
keromian <keromian@gmail.com>
2023-07-18 15:20:52 +0900

    마켓컬리 자바기반 리액트 프로젝트

---
+++ src/components/wrap/main_sub_bbs/NoticeWriteFormPageComponent.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './notice_scss/notice_write.scss';
+import NoticeLeftNavComponent from './NoticeComponent/NoticeLeftNavComponent';
+import axios from 'axios';
+import { GlobalContext } from '../../../context/GlobalContext';
+
+export default function NoticeWriteFormPageComponent () {
+
+    const [sign, setSign]=React.useContext(GlobalContext);
+
+    const [state, setState] = React.useState({
+        제목:'',
+        내용:''
+    });
+
+    const onSubmitWrite=(e)=>{
+        e.preventDefault();
+        axios({
+            url:'/bbs/writeAction.jsp',
+            method:'POST',
+            data: {},
+            params: {
+            //    "bbsId": state.번호,  로그인 id 가져오기
+               "subject": state.제목, 
+               "content": state.내용
+           }
+        })
+        .then((res)=>{            
+            if(res.status===200 && res.data !== -1 ){
+                alert('글이 등록 되었습니다.');
+                window.location.pathname = '/notice';
+            }
+            else{
+               alert('수정실패 데이터 확인하고 다시 시도하세요');
+            }
+        })
+        .catch((err)=>{
+            console.log('AXIOS 실패!' + err );
+        });
+        
+
+
+    }
+
+    const onChangeSubject=(e)=>{
+        setState({
+            ...state,
+            subject: e.target.value,
+        })
+    }
+    const onChangeContents=(e)=>{
+        setState({
+            ...state,
+            contents: e.target.value,
+        })
+    }
+
+
+    return (
+        <div id='noticeWrite' className='noticeWrite'>
+            <div className="container">
+                <div className="left">
+                    <NoticeLeftNavComponent/>
+                </div>
+                <div className="right">
+                    <div className="title">
+                        <h2>공지사항</h2>
+                    </div>
+                    <div className="content">
+                        <form onSubmit={onSubmitWrite}>
+                            <ul>
+                                <li>
+                                    <label htmlFor="subject">제목<i>*</i></label>
+                                    <input 
+                                    onChange={onChangeSubject}
+                                    type="text" 
+                                    name='subject' 
+                                    id='subject' 
+                                    value={state.제목} 
+                                    placeholder='제목을 입력해 주세요' 
+                                    />
+                                </li>
+                                <li>
+                                    <label htmlFor="contents">내용<i>*</i></label>
+                                    <textarea 
+                                    onChange={onChangeContents}
+                                    name="contents" 
+                                    id="contents"  
+                                    placeholder='내용을 입력해 주세요' 
+                                    value={state.내용}
+                                    ></textarea>
+                                </li>
+                            </ul> 
+
+                            <div className="button-box">
+                                <button type='submit'>등록</button> 
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
